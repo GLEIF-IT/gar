3b3045120e94cffeff9ae58f880b6970ddde2ec6
Kevin Griffin <griffin.kev@gmail.com>
2022-11-09 16:07:50 -0500

    adds new durls and QVI schema

--- scripts/issue-qvi-credential.sh
+++ scripts/issue-qvi-credential.sh
@@ -17,4 +17,4 @@ read -p "Enter the alias of the new QVI: " -r recipient
 
 echo \"${lei}\" | jq -f "${EXT_GAR_SCRIPT_DIR}/qvi-data.jq" > "${EXT_GAR_DATA_DIR}/qvi-data.json"
 
-kli vc issue --name "${EXT_GAR_NAME}" --passcode "${passcode}" --alias "${EXT_GAR_ALIAS}" --registry-name "${EXT_GAR_REG_NAME}" --schema ELqriXX1-lbV9zgXP4BXxqJlpZTgFchll3cyjaCyVKiz --recipient "${recipient}" --data @"${EXT_GAR_DATA_DIR}/qvi-data.json"
+kli vc issue --name "${EXT_GAR_NAME}" --passcode "${passcode}" --alias "${EXT_GAR_ALIAS}" --registry-name "${EXT_GAR_REG_NAME}" --schema EBfdlu8R27Fbx-ehrqwImnK-8Cm79sqbAQ4MmvEAYqao --recipient "${recipient}" --data @"${EXT_GAR_DATA_DIR}/qvi-data.json"
