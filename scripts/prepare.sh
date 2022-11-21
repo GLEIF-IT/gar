5bc54a195cd3946f718f541b3a5ded534a9a9c18
Kevin Griffin <griffin.kev@gmail.com>
2022-11-21 15:13:26 -0500

    bumps gleif/keri version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,7 +6,7 @@
 ##################################################################
 
 # Pull container required to run all KERI/ACDC commands
-docker pull gleif/keri:0.6.8
+docker pull gleif/keri:0.6.9
 
 # Create local directory for datastore, keystore and configuration
 mkdir -p "${HOME}"/.gar/cf
