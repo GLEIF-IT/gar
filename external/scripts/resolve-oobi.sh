9b694463a94d6786bb0260239b99c0143ab51566
pfeairheller <pfeairheller@gmail.com>
2022-11-30 13:00:14 -0800

    Add option to resolve OOBI to leave off oobi alias
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

--- external/scripts/resolve-oobi.sh
+++ external/scripts/resolve-oobi.sh
@@ -15,6 +15,13 @@ passcode="$(security find-generic-password -w -a "${LOGNAME}" -s ext-gar-passcod
 read -p "Type or paste OOBI URL: " -r oobi_url
 read -p "Enter an Alias for the AID: " -r alias
 echo " "
-kli oobi resolve --name "${EXT_GAR_NAME}" --passcode "${passcode}" --oobi-alias "${alias}" --oobi "${oobi_url}"
-echo " "
-kli contacts list --name "${EXT_GAR_NAME}" --passcode "${passcode}" | jq "if .alias == \"${alias}\" then \"Alias: \"+.alias+\"\n\rAID:   \"+.id  +\"\n\r\" else \"\" end" --raw-output
+
+if [ -z "$alias" ]
+then
+  kli oobi resolve --name "${EXT_GAR_NAME}" --passcode "${passcode}" --oobi "${oobi_url}"
+else
+  kli oobi resolve --name "${EXT_GAR_NAME}" --passcode "${passcode}" --oobi-alias "${alias}" --oobi "${oobi_url}"
+  echo " "
+  kli contacts list --name "${EXT_GAR_NAME}" --passcode "${passcode}" | jq "if .alias == \"${alias}\" then \"Alias: \"+.alias+\"\n\rAID:   \"+.id  +\"\n\r\" else \"\" end" --raw-output
+fi
+
