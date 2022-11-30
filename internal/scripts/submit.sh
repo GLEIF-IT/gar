f783df27769cde201b20e49a82273e7cf08ce9e5
pfeairheller <pfeairheller@gmail.com>
2022-11-30 08:21:56 -0800

    Added submit script for resubmitting an AID to witnesses for receipts.
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

---
+++ internal/scripts/submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################################################################
+##                                                              ##
+##      Script for continuing a multisig event process          ##
+##                                                              ##
+##################################################################
+
+PWD=$(pwd)
+source $PWD/source.sh
+
+# Capture password
+passcode="$(security find-generic-password -w -a "${LOGNAME}" -s int-gar-passcode)"
+
+read -p "Enter the Alias to submit: " -r alias
+
+kli witness submit --name "${EXT_GAR_NAME}" --passcode "${passcode}" --alias "${alias}"
