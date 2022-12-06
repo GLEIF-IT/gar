d0bb0ce08f26d061ae1be28fdbe35d979784397b
pfeairheller <pfeairheller@gmail.com>
2022-12-05 18:07:07 -0800

    Generate nonce in create-registry scripts.
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

--- external/scripts/create-registry.sh
+++ external/scripts/create-registry.sh
@@ -12,6 +12,17 @@ source $PWD/source.sh
 # Capture password and salt
 passcode="$(security find-generic-password -w -a "${LOGNAME}" -s ext-gar-passcode)"
 
+echo "Do you need to create nonce?"
+read -p "[y/N] " -r yn
+case $yn in
+  "Y" | "y")
+    kli nonce
+    ;;
+  *)
+    ;;
+esac
+echo ""
+
 read -p "Type or paste a nonce: " -r nonce
 
 kli vc registry incept  --name "${EXT_GAR_NAME}" --passcode "${passcode}"  --alias "${EXT_GAR_AID_ALIAS}" --registry-name "${EXT_GAR_REG_NAME}" --nonce "${nonce}"
--- internal/scripts/create-registry.sh
+++ internal/scripts/create-registry.sh
@@ -12,6 +12,17 @@ source $PWD/source.sh
 # Capture password and salt
 passcode="$(security find-generic-password -w -a "${LOGNAME}" -s int-gar-passcode)"
 
+echo "Do you need to create nonce?"
+read -p "[y/N] " -r yn
+case $yn in
+  "Y" | "y")
+    kli nonce
+    ;;
+  *)
+    ;;
+esac
+echo ""
+
 read -p "Type or paste a nonce: " -r nonce
 
 kli vc registry incept  --name "${INT_GAR_NAME}" --passcode "${passcode}"  --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --nonce "${nonce}"
