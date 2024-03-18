fc9a73da0b30b09e3cebf6306c27bbc42e4ad4dd
pfeairheller <pfeairheller@gmail.com>
2024-03-18 13:25:32 -0700

    Pushed internal gar scripts with updates to work with IPEX.

--- internal/scripts/issue-ecr-auth-credential.sh
+++ internal/scripts/issue-ecr-auth-credential.sh
@@ -23,14 +23,17 @@ echo "[\"${AID}\", \"${lei}\", \"${personLegalName}\", \"${engagementContextRole
 read -p "Enter AID of QVI : " -r recipient
 
 # Prepare the EDGES Section
-le_said=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said --schema ENPXp1vQzRF6JwIuS-mp2U8Uf1MoADoP_GqQ62VsDZWY  | tr -d '\r')
+le_said=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said --schema ENPXp1vQzRF6JwIuS-mp2U8Uf1MoADoP_GqQ62VsDZWY  | tr -d '\r' | sed -n '1 p')
 
 echo "\"${le_said}\"" | jq -f "${INT_GAR_SCRIPT_DIR}/ecr-auth-edges-filter.jq" > "${INT_GAR_DATA_DIR}/ecr-auth-edge-data.json"
 kli saidify --file /data/ecr-auth-edge-data.json
 
+# Prepare the RULES section
+cp "${INT_GAR_SCRIPT_DIR}/ecr-rules.json" "${INT_GAR_DATA_DIR}/ecr-rules.json"
+
 # wip
-kli vc create --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --schema ED_PcIn1wFDe0GB0W7Bk9I4Q_c9bQJZCM2w7Ex9Plsta --recipient "${recipient}" --data @"/data/ecr-auth-data.json" --edges @"/data/ecr-auth-edge-data.json" --rules @"/data/rules.json"
+kli vc create --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --schema EH6ekLjSr8V32WyFbGe1zXjTzFs9PkTYmupJ9H65O14g --recipient "${recipient}" --data @"/data/ecr-auth-data.json" --edges @"/data/ecr-auth-edge-data.json" --rules @"/data/ecr-rules.json"  --time 2024-03-18T14:52:55+00:00
 
-SAID=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --issued --said --schema ED_PcIn1wFDe0GB0W7Bk9I4Q_c9bQJZCM2w7Ex9Plsta)
+SAID=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --issued --said --schema EH6ekLjSr8V32WyFbGe1zXjTzFs9PkTYmupJ9H65O14g)
 
-kli ipex grant --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said "${SAID}"
+kli ipex grant --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said "${SAID}" --time 2024-03-18T14:52:55+00:00 --recipient "${recipient}"
--- internal/scripts/issue-oor-auth-credential.sh
+++ internal/scripts/issue-oor-auth-credential.sh
@@ -23,7 +23,8 @@ read -p "Enter the Alias of the QVI to authorize with this AUTH credential: " -r
 echo "[\"${AID}\", \"${lei}\", \"${personLegalName}\", \"${officialRole}\"]" | jq -f "${INT_GAR_SCRIPT_DIR}/oor-auth-data.jq" > "${INT_GAR_DATA_DIR}/oor-auth-data.json"
 
 # Prepare the EDGES Section
-le_said=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said --schema ENPXp1vQzRF6JwIuS-mp2U8Uf1MoADoP_GqQ62VsDZWY  | tr -d '\r')
+le_said=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said --schema ENPXp1vQzRF6JwIuS-mp2U8Uf1MoADoP_GqQ62VsDZWY  | tr -d '\r' | sed -n '2 p')
+echo "${le_said}"
 
 echo "\"${le_said}\"" | jq -f "${INT_GAR_SCRIPT_DIR}/oor-auth-edges-filter.jq" > "${INT_GAR_DATA_DIR}/oor-auth-edge-data.json"
 kli saidify --file /data/oor-auth-edge-data.json
@@ -31,8 +32,8 @@ kli saidify --file /data/oor-auth-edge-data.json
 # Prepare the RULES section
 cp "${INT_GAR_SCRIPT_DIR}/rules.json" "${INT_GAR_DATA_DIR}/rules.json"
 
-kli vc create --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --schema EKA57bKBKxr_kN7iN5i7lMUxpMG-s19dRcmov1iDxz-E --recipient "${recipient}" --data @"/data/oor-auth-data.json" --edges @"/data/oor-auth-edge-data.json" --rules @"/data/rules.json"
+kli vc create --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --schema EKA57bKBKxr_kN7iN5i7lMUxpMG-s19dRcmov1iDxz-E --recipient "${recipient}" --data @"/data/oor-auth-data.json" --edges @"/data/oor-auth-edge-data.json" --rules @"/data/rules.json" --time 2024-03-18T14:52:55+00:00
 
 SAID=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --issued --said --schema EKA57bKBKxr_kN7iN5i7lMUxpMG-s19dRcmov1iDxz-E)
 
-kli ipex grant --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said "${SAID}"
+kli ipex grant --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said "${SAID}" --time 2024-03-18T14:52:55+00:00
