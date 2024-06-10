5fe536eaa0a7bfd1de8283ed25335e583c0d7b5d
Kevin Griffin <griffin.kev@gmail.com>
2024-06-10 15:12:26 -0400

    adds updates for oor
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- internal/scripts/create-oor-auth-credential.sh
+++ internal/scripts/create-oor-auth-credential.sh
@@ -2,7 +2,7 @@
 
 ##################################################################
 ##                                                              ##
-##          Script for issuing ecr auth                         ##
+##          Script for issuing oor auth                         ##
 ##                                                              ##
 ##################################################################
 
@@ -13,7 +13,7 @@ source $PWD/source.sh
 passcode="$(security find-generic-password -w -a "${LOGNAME}" -s int-gar-passcode)"
 
 read -p "Enter your LEI : " -r lei
-read -p "Enter or Paste the AID of the recipient of the ECR credential: " -r AID
+read -p "Enter or Paste the AID of the recipient of the OOR credential: " -r AID
 read -p "Enter requested person legal name: " -r personLegalName
 read -p "Enter requested engagement context role: " -r engagementContextRole
 read -p "Enter the Alias of the QVI to authorize with this AUTH credential: " -r recipient
