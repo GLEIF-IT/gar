7ac1f8227bd801e981443b15dee43d554540fbcd
Kevin Griffin <griffin.kev@gmail.com>
2024-06-10 14:57:04 -0400

    adds updates for oor
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- internal/scripts/create-oor-auth-credential.sh
+++ internal/scripts/create-oor-auth-credential.sh
@@ -20,15 +20,15 @@ read -p "Enter the Alias of the QVI to authorize with this AUTH credential: " -r
 read -p "Enter the datetime to use: " -r datetime
 
 # Prepare DATA Section
-echo "[\"${AID}\", \"${lei}\", \"${personLegalName}\", \"${engagementContextRole}\"]" | jq -f "${INT_GAR_SCRIPT_DIR}/ecr-auth-data.jq" > "${INT_GAR_DATA_DIR}/ecr-auth-data.json"
+echo "[\"${AID}\", \"${lei}\", \"${personLegalName}\", \"${officialRole}\"]" | jq -f "${INT_GAR_SCRIPT_DIR}/oor-auth-data.jq" > "${INT_GAR_DATA_DIR}/oor-auth-data.json"
 
 # Prepare the EDGES Section
 le_said=EB9yGbWXOn_MhTQsHltftAsNrlWAxgedMPIQl4rg6C1e
 
-echo "\"${le_said}\"" | jq -f "${INT_GAR_SCRIPT_DIR}/ecr-auth-edges-filter.jq" > "${INT_GAR_DATA_DIR}/ecr-auth-edge-data.json"
-kli saidify --file /data/ecr-auth-edge-data.json
+echo "\"${le_said}\"" | jq -f "${INT_GAR_SCRIPT_DIR}/oor-auth-edges-filter.jq" > "${INT_GAR_DATA_DIR}/oor-auth-edge-data.json"
+kli saidify --file /data/oor-auth-edge-data.json
 
 # Prepare the RULES section
-cp "${INT_GAR_SCRIPT_DIR}/ecr-rules.json" "${INT_GAR_DATA_DIR}/ecr-rules.json"
+cp "${INT_GAR_SCRIPT_DIR}/oor-rules.json" "${INT_GAR_DATA_DIR}/oor-rules.json"
 
-kli vc create --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --schema EH6ekLjSr8V32WyFbGe1zXjTzFs9PkTYmupJ9H65O14g --recipient "${recipient}" --data @"/data/ecr-auth-data.json" --edges @"/data/ecr-auth-edge-data.json" --rules @"/data/ecr-rules.json"  --time "${datetime}"
+kli vc create --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --registry-name "${INT_GAR_REG_NAME}" --schema EKA57bKBKxr_kN7iN5i7lMUxpMG-s19dRcmov1iDxz-E --recipient "${recipient}" --data @"/data/oor-auth-data.json" --edges @"/data/oor-auth-edge-data.json" --rules @"/data/oor-rules.json" --time "${datetime}"
