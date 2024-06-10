c587e665f77cc2c51e78899f130a22ac2385f6cc
Kevin Griffin <griffin.kev@gmail.com>
2024-06-10 13:44:23 -0400

    wip
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- internal/scripts/grant-ecr-auth-credential.sh
+++ internal/scripts/grant-ecr-auth-credential.sh
@@ -14,7 +14,6 @@ passcode="$(security find-generic-password -w -a "${LOGNAME}" -s int-gar-passcod
 
 read -p "Enter the Alias of the QVI to authorize with this AUTH credential: " -r recipient
 read -p "Enter the datetime to use: " -r datetime
+read -p "Enter the Auth credential SAID: " -r SAID
 
-SAID=$(kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --issued --said --schema EH6ekLjSr8V32WyFbGe1zXjTzFs9PkTYmupJ9H65O14g)
-export DEBUG_KLI=1
 kli ipex grant --name "${INT_GAR_NAME}" --passcode "${passcode}" --alias "${INT_GAR_AID_ALIAS}" --said "${SAID}" --time "${datetime}" --recipient "${recipient}"
