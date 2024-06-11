f8131e50b8fb76c6aa0f19e489e6f9334fdfe716
Kevin Griffin <griffin.kev@gmail.com>
2024-06-11 07:48:14 -0400

    adds updates for oor
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- internal/scripts/create-oor-auth-credential.sh
+++ internal/scripts/create-oor-auth-credential.sh
@@ -15,7 +15,7 @@ passcode="$(security find-generic-password -w -a "${LOGNAME}" -s int-gar-passcod
 read -p "Enter your LEI : " -r lei
 read -p "Enter or Paste the AID of the recipient of the OOR credential: " -r AID
 read -p "Enter requested person legal name: " -r personLegalName
-read -p "Enter requested engagement context role: " -r engagementContextRole
+read -p "Enter requested official role: " -r officialRole
 read -p "Enter the Alias of the QVI to authorize with this AUTH credential: " -r recipient
 read -p "Enter the datetime to use: " -r datetime
 
