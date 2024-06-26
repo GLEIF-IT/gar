934991077af4726bf0c66ab4d1dbb274889725be
Kevin Griffin <griffin.kev@gmail.com>
2024-06-26 08:33:39 -0500

    debug
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- internal/scripts/list-credentials.sh
+++ internal/scripts/list-credentials.sh
@@ -13,4 +13,5 @@ source $PWD/source.sh
 passcode="$(security find-generic-password -w -a "${LOGNAME}" -s int-gar-passcode)"
 
 # Here's your credentials:
+export DEBUG_KLI=1
 kli vc list --name "${INT_GAR_NAME}" --passcode "${passcode}" --poll "$@"
