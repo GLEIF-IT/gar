e2400ed2dad89ff24ad9981c2bce221a4d8f98bc
Kevin Griffin <griffin.kev@gmail.com>
2025-02-23 20:11:15 -0500

    version bump
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- external/source.sh
+++ external/source.sh
@@ -32,7 +32,7 @@ function kli() {
     -v "${EXT_GAR_DATA_DIR}":/data \
     -e PYTHONWARNINGS="ignore::SyntaxWarning" \
     -e DEBUG_KLI="${DEBUG}" \
-    weboftrust/keri:1.1.30 "$@"
+    weboftrust/keri:1.1.31 "$@"
 }
 
 export -f kli
